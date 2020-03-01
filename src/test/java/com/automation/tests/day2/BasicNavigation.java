926905eb21e092c834eea70d019b85ee96f79ab2
scokavci <scokavci@gmail.com>
2020-03-01 13:54:48 -0500

    test title

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -16,7 +16,18 @@ public class BasicNavigation {
         // In selenium , everything starts from WebDriver interface
         // ChromeDriver extends RemoteWebDriver --> implements WebDriver
         driver.get("http://google.com");  //to open website
+
         Thread.sleep(3000);  // for demo, wait 3 seconds
+        // method that return page title
+        String title = driver.getTitle();  // returns <title>Some title</title/ text
+        String expextedTitle = "Google";
+
+        System.out.println("Title is..."+title);
+        if(expextedTitle.equals(title)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
         driver.close(); // to close browser
 
     }
