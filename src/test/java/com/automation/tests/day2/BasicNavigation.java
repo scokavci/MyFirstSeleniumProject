ce8aa62a32eb08285251cb00a10454b43f0ae6ad
scokavci <scokavci@gmail.com>
2020-03-01 13:42:43 -0500

    open and close website

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //to start selenium script we need
         //setup webdriver (browser driver) and create webdriver object
         //WebDriverManager is abstract class
@@ -14,8 +14,10 @@ public class BasicNavigation {
         // ChromeDriver is class;WebDriver is Interface
         WebDriver driver = new ChromeDriver();  // object creation
         // In selenium , everything starts from WebDriver interface
+        // ChromeDriver extends RemoteWebDriver --> implements WebDriver
         driver.get("http://google.com");  //to open website
-
+        Thread.sleep(3000);  // for demo, wait 3 seconds
+        driver.close(); // to close browser
 
     }
 }
